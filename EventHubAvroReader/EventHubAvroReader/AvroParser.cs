d564f97a4eb84da33b8e550fa66bf1ccc6780e8b
Marcus McConnell <marcus@bvsoftware.com>
2018-01-23 18:25:29 -0500

    First Commit

---
+++ EventHubAvroReader/EventHubAvroReader/AvroParser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace EventHubAvroReader
+{
+    public class AvroParser
+    {
+        public static List<EventHubAvroData> ParseAvroFile(string fileName)
+        {
+            if (!File.Exists(fileName))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Error: Missing Avro file " + fileName);
+                Console.ResetColor();
+            }
+         
+            List<EventHubAvroData> items = new List<EventHubAvroData>();
+
+            using (var reader = Avro.File.DataFileReader<object>.OpenReader(fileName))
+            {
+                foreach (Avro.Generic.GenericRecord m in reader.NextEntries)
+                {
+                    EventHubAvroData e = new EventHubAvroData(m);
+                    items.Add(e);
+                }
+            }
+
+            return items;
+        }
+    }
+}
---
+++ EventHubAvroReader/EventHubAvroReader/EventHubAvroData.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventHubAvroReader
+{
+    public class EventHubAvroData
+    {
+        public DateTime EnqueuedTimeUtc { get; set; }
+        public long SequenceNumber { get; set; }
+        public string Offset { get; set; }
+        public byte[] Body { get; set; }
+
+        public EventHubAvroData()
+        {
+
+        }
+
+        public EventHubAvroData(Avro.Generic.GenericRecord record)
+        {
+            this.SequenceNumber = Parse<long>("SequenceNumber", record);
+            this.Offset = Parse<string>("Offset", record);
+            string dateString = Parse<string>("EnqueuedTimeUtc", record);
+
+            DateTime enq;
+            if (DateTime.TryParse(dateString, out enq) == true)
+            {
+                EnqueuedTimeUtc = enq;
+            }
+
+            this.Body = Parse<byte[]>("Body", record);
+
+        }
+
+        private T Parse<T>(string fieldName, Avro.Generic.GenericRecord record)
+        {
+            try
+            {
+                object data;
+                record.TryGetValue(fieldName, out data);
+                return (T)data;
+            }
+            catch
+            {
+                return default(T);
+            }
+
+        }
+
+        public string BodyAsString
+        {
+            get
+            {
+                string result = "";
+
+                try
+                {
+                    result = Encoding.UTF8.GetString(this.Body);
+                }
+                catch
+                {
+                    result = "Error Decoding!";
+                }
+
+                return result;
+            }
+        }
+    }
+}
