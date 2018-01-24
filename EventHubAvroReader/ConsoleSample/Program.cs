2ddd1679c8c324f384bbffb93fc5b44b5eeaecdf
Marcus McConnell <marcus@bvsoftware.com>
2018-01-23 19:13:30 -0500

    Added input type of stream as alternate to filename. Added .NET Core sample console app:

---
+++ EventHubAvroReader/ConsoleSample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                string fileName = args[0];
+                var items = EventHubAvroReader.AvroParser.ParseAvroFile(fileName);
+                Console.WriteLine("Parsed {0} items", items.Count);
+                foreach(var item in items)
+                {
+                    Console.WriteLine("Enqueued Time: " + item.EnqueuedTimeUtc.ToLongDateString());
+                }
+            }
+            else
+            {
+                Console.WriteLine("Missing input file name!");
+            }
+
+            //Console.ReadKey();
+        }
+    }
+}
--- EventHubAvroReader/EventHubAvroReader/AvroParser.cs
+++ EventHubAvroReader/EventHubAvroReader/AvroParser.cs
@@ -15,10 +15,18 @@ namespace EventHubAvroReader
                 Console.WriteLine("Error: Missing Avro file " + fileName);
                 Console.ResetColor();
             }
-         
+
+            using (Stream f = File.Open(fileName, FileMode.Open))
+            {
+                return ParseAvroFile(f);
+            }
+        }
+        public static List<EventHubAvroData> ParseAvroFile(Stream inStream)
+        {
+           
             List<EventHubAvroData> items = new List<EventHubAvroData>();
 
-            using (var reader = Avro.File.DataFileReader<object>.OpenReader(fileName))
+            using (var reader = Avro.File.DataFileReader<object>.OpenReader(inStream))
             {
                 foreach (Avro.Generic.GenericRecord m in reader.NextEntries)
                 {
